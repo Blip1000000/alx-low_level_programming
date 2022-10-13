528ea6c15853ac5c19540e2dbaffcca58fec2116
Blip1000000 <Blip1000000@gmail.com>
2022-10-13 13:08:30 -0700

    add1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,6 @@
  *Return: By default returns zero if no erros otherwise returns non-zero value */
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
-
